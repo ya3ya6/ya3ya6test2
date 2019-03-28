59dc7db3a3f420e91df541f4cd8173550042045e
unknown <ya3ya63@gmail.com>
2019-03-29 04:17:32 +0430

    commit4

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 var jalaali = require('jalaali-js')
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 3000
 //use pug as template engine
 app.set('view engine', 'pug')
 
